fa9d106a231ea76b7a7ecc0c136263033581ac4a
moddebaixa <moddebaixa@gmail.com>
2024-05-15 18:50:27 +0200

    Initialize project using Create React App

---
+++ src/componentes/sprinter.jsx
@@ -0,0 +1,25 @@
+// import React from 'react'
+
+const sprinter = () => {
+    return (
+        <>
+            <section className='sectionWraper'>
+                <div className='row'>
+                    <div className="col bg-12 md-12 sm-12">
+                        <p>hello</p>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col bg-6 md-6 sm-12'>
+                        <p>hello world</p>
+                    </div>
+                    <div className='col bg-6 md-6 sm-12'>
+                        <p>hello world</p>
+                    </div>
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default sprinter;
